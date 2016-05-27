de85a4e3dbf8f0bde4c22c6ee9633cfc5ed67030
Jaemi Bremner <jaemi.swope@gmail.com>
2016-05-26 18:42:19 -0700

    Update to JS for homework.

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ $(document).ready(function (){
 
       //copy update on Arun's instructions
     //   console.log (users[0].firstname + users[0].lastname + "'s user name is " + users[0].username + "and their email is " + users[0].email);
-    // },
     // error: function () {
     //   alert("Error getting data");
     // }
-  });
-});
 
+    console.log ("The wine’s name is " + wines[0].name + ".  The vintage year is " + wines[0].year + ". It is from "  wines[0].country + " " + wines[0].region + " made of " + wines[0].grapes + "grapes.  It is described as" + wines[0].description + ".  The cost is $" + wines[0].price + ".");
+
+  },
+  error: function () {
+    alert("Error getting data");
+  }
+});
 });
